3df05f2f95868451f4120ba3118c3eb33ad63891
Stackbit Code Editor <projects@stackbit.com>
2021-10-24 10:15:38 +0000

    _document.js: changed

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -9,7 +9,7 @@ export default class MyDocument extends Document {
         return { ...initialProps, helmet: Helmet.renderStatic() };
     }
 
-    // should render on <html>
+    // должен отображаться на <html>
     get helmetHtmlAttrComponents() {
         return this.props.helmet.htmlAttributes.toComponent();
     }
