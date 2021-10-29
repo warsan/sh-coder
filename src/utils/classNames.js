bacf61c4a652ffee2b3f27dca03f29875de39493
Stackbit Code Editor <projects@stackbit.com>
2021-10-29 02:10:26 +0000

    classNames.js: changed

--- src/utils/classNames.js
+++ src/utils/classNames.js
@@ -1,6 +1,6 @@
 import classnames from 'classnames';
 
-// Простая обертка вокруг classNames для возврата null, если классы не были созданы
+// Простая обёртка вокруг classNames для возврата null, если классы не были созданы
 // Otherwise, original classNames returns empty string which causes class="" to be generated
 export default function classNames(...args) {
     return classnames.call(this, ...args) || null;
